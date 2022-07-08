686ca3ec26489641b374f076ebdcc19ccca35f55
Fut <shmopls@gmail.com>
2022-07-08 08:43:53 +0500

    Парсинг HTML страницы

--- src/main/java/parser/HabrCareerParse.java
+++ src/main/java/parser/HabrCareerParse.java
@@ -6,7 +6,6 @@ import org.jsoup.Connection;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
 
