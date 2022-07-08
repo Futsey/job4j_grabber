e1da4e2ae9bf7eb2fbc87c1a72d544f08b196e61
Fut <shmopls@gmail.com>
2022-07-08 08:35:13 +0500

    Парсинг HTML страницы

--- src/main/java/parser/HabrCareerParse.java
+++ src/main/java/parser/HabrCareerParse.java
@@ -6,16 +6,17 @@ import org.jsoup.Connection;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
 
 public class HabrCareerParse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
-
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
+        HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
         Connection connection = Jsoup.connect(PAGE_LINK);
         Document document = connection.get();
         Elements rows = document.select(".vacancy-card__inner");
@@ -23,11 +24,10 @@ public class HabrCareerParse {
             Element titleElement = row.select(".vacancy-card__title").first();
             Element linkElement = titleElement.child(0);
             Element titleDate = row.select(".vacancy-card__date").first();
-            Element linkDate = titleElement.child(0);
-            String vacancyName = linkDate.text();
-            String vacancyDate = titleDate.text();
-            String link = String.format("%s%s%s", SOURCE_LINK, linkElement.attr("href"), linkDate.attr("time"));
-            System.out.printf("%s %s %s%n", vacancyName, link, vacancyDate);
+            Element linkDate = titleDate.child(0);
+            String vacancyName = linkElement.text();
+            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"), linkDate.attr("time"));
+            System.out.printf("%s %s %s%n", vacancyName, link, linkDate);
         });
     }
 }
