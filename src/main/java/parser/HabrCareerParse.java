fbc4d2a829ad77af7189a9a1486311c984be019a
Fut <shmopls@gmail.com>
2022-07-12 09:36:34 +0500

    SqlRuParse

--- src/main/java/parser/HabrCareerParse.java
+++ src/main/java/parser/HabrCareerParse.java
@@ -8,16 +8,19 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
     private static final String PAGE_NUMBER = "?page=";
-    private static final int PAGES_TO_PARSE = 5;
+    private static final int PAGES_TO_PARSE = 1;
 
     public static void main(String[] args) throws IOException {
         HabrCareerParse myCreer = new HabrCareerParse();
+        HabrCareerDateTimeParser timeParser = new HabrCareerDateTimeParser();
         for (int i = 0; i <= PAGES_TO_PARSE; i++) {
             Connection connection = Jsoup.connect(PAGE_LINK + PAGE_NUMBER + i);
             Document document = connection.get();
@@ -32,6 +35,15 @@ public class HabrCareerParse {
                 String link = String.format("%s%s", SOURCE_LINK,
                         linkElement.attr("href"),
                         linkDate.attr("time"));
+
+                int id = 0;
+                Post post = new Post(++id,
+                        vacancyName,
+                        link,
+                        myCreer.retrieveDescription(link),
+                        timeParser.parse(date));
+                myCreer.list(post);
+
                 System.out.printf("%s" + " | " +  "%s" + " | " +  "%s" + System.lineSeparator() +  "%s%n%n",
                         vacancyName,
                         link,
@@ -51,5 +63,13 @@ public class HabrCareerParse {
             e.printStackTrace();
         }
         return link;
+    }
+
+    @Override
+    public List<Post> list(Post post) {
+        List<Post> jobList = new ArrayList<>();
+        jobList.add(post);
+        System.out.println(post);
+        return jobList;
     }
 }
--- src/main/java/parser/Parse.java
+++ src/main/java/parser/Parse.java
@@ -4,5 +4,5 @@ import java.util.List;
 
 public interface Parse {
 
-    List<Post> list(String link);
+    List<Post> list(Post post);
 }
